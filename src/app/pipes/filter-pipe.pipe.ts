6b4c084d84ef16a607751cc8585b7fd6338a0437
gorkemetem <gorkemetem98@gmail.com>
2022-04-24 12:56:48 +0300

    Pipes added

---
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -0,0 +1,16 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { Pipe, PipeTransform } from '@angular/core';
+import { filter } from 'rxjs';
+import { Product } from '../models/product';
+
+@Pipe({
+  name: 'filterPipe'
+})
+export class FilterPipePipe implements PipeTransform {
+
+  transform(value: Product[], filterText: string): Product[] {
+    filterText = filterText?filterText.toLocaleLowerCase():""
+    return filterText?value.filter((p:Product) => p.productName.toLocaleLowerCase().indexOf(filterText)!==-1):value;
+  }
+
+}
